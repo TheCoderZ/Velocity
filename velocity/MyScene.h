222a1a5ed203490debf31c4e8bff2c987bf394c4
TheRealAphex <thearstar@gmail.com>
2014-02-12 16:19:58 -0600

    Initial Commit

---
+++ velocity/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  velocity
+//
+
+//  Copyright (c) 2014 Adil Rahman. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ velocity/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  velocity
+//
+
+//  Copyright (c) 2014 Adil Rahman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
